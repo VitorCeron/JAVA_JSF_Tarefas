58a690c70ca8ea67d66e596cdfa5b1d92556d351
Vitor Ceron <vitor.ceron1997@gmail.com>
2019-04-01 22:29:31 -0300

    Commit inicial do projeto

---
+++ src/java/br/com/tarefa/bean/TarefaBean.java
@@ -0,0 +1,42 @@
+
+package br.com.tarefa.bean;
+import br.com.tarefa.entidade.Tarefa;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author vitor
+ */
+@ManagedBean
+@SessionScoped
+public class TarefaBean {
+
+    private Tarefa tarefa = new Tarefa();
+    private List<Tarefa> tarefas = new ArrayList<>();
+    
+    public void adicionar(){
+        tarefas.add(tarefa);
+        tarefa = new Tarefa();
+    }
+
+    public Tarefa getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(Tarefa tarefa) {
+        this.tarefa = tarefa;
+    }
+
+    public List<Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    public void setTarefas(List<Tarefa> tarefas) {
+        this.tarefas = tarefas;
+    }
+    
+    
+}
---
+++ src/java/br/com/tarefa/entidade/Tarefa.java
@@ -0,0 +1,64 @@
+package br.com.tarefa.entidade;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author vitor
+ */
+public class Tarefa {
+    
+    private String idTarefa;
+    private String tituloTarefa;
+    private String descricaoTarefa;
+
+    public String getIdTarefa() {
+        return idTarefa;
+    }
+
+    public void setIdTarefa(String idTarefa) {
+        this.idTarefa = idTarefa;
+    }
+
+    public String getTituloTarefa() {
+        return tituloTarefa;
+    }
+
+    public void setTituloTarefa(String tituloTarefa) {
+        this.tituloTarefa = tituloTarefa;
+    }
+
+    public String getDescricaoTarefa() {
+        return descricaoTarefa;
+    }
+
+    public void setDescricaoTarefa(String descricaoTarefa) {
+        this.descricaoTarefa = descricaoTarefa;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Tarefa other = (Tarefa) obj;
+        if (!Objects.equals(this.idTarefa, other.idTarefa)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
